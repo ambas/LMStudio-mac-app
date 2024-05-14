9a21d7ff024c4b45fa6422e31af0c20cee169c78
Ambas <ambaschbsanti@gmail.com>
2024-05-14 23:35:27 +0900

    Initial Commit

---
+++ LMStudio-mac-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LMStudio-mac-app
+//
+//  Created by Ambas Chobsanti on 2024/05/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LMStudio-mac-app/LMStudio_mac_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  LMStudio_mac_appApp.swift
+//  LMStudio-mac-app
+//
+//  Created by Ambas Chobsanti on 2024/05/14.
+//
+
+import SwiftUI
+
+@main
+struct LMStudio_mac_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
